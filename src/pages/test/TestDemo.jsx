99004c3625c0d3f921fadc053a724fdbd15e76d3
zhaoyang <1320587103@qq.com>
2019-10-10 14:45:41 +0800

    umi小demo第一次提交代码

---
+++ src/pages/test/TestDemo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class TestDemo extends React.Component{
+    render(){
+        return(
+            <div>
+                我是练手的页面
+            </div>
+        )
+    }
+}
